2ef376f58021bca6a193f31d38974d0e45b4fc4a
Daniel Andrés Castillo Ardila <danielcastilloardila@Daniels-MacBook-Air.local>
2024-07-18 19:41:46 -0500

    added queue integration with ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { SQSClient, ListQueuesCommand, SendMessageCommand, SendMessageCommandOutput, SendMessageCommandInput } from "@aws-sdk/client-sqs";
 import UserModel from './models/user.model';
 import { StatusCodes } from 'http-status-codes';
 import { MongooseError } from 'mongoose';
@@ -6,6 +7,8 @@ import './config/database'; // Import the singleton instance to ensure the DB co
 
 const app = express();
 app.use(express.json()) // for parsing application/json
+
+const client = new SQSClient({ region: "us-east-1" });
 
 app.post('/user', async (req: Request, res: Response) => {
     try {
@@ -23,6 +26,18 @@ app.post('/user', async (req: Request, res: Response) => {
 
     }
 });
+
+app.post('/queue', async (req: Request, res: Response) => {
+    const { user } = req.body;
+    const result = await sendMessageToSqs({ MessageBody: JSON.stringify(user), QueueUrl: '' })
+    res.status(StatusCodes.CREATED).json(result)
+});
+
+
+const sendMessageToSqs = async (data: SendMessageCommandInput): Promise<SendMessageCommandOutput> => {
+    const command = new SendMessageCommand(data);
+    return await client.send(command);
+}
 
 app.listen(3000, () => {
     console.log('server running')
